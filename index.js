27a35f74460b0a7accfab8e776eed809e2ba4cb0
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-08-07 23:46:57 +0530

    Fixed issue

--- index.js
+++ index.js
@@ -57,7 +57,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+
