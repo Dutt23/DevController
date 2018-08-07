2600a922e6e1adb6ccaec53b9b219919d9416a21
shatyaki_dutt@hotmail.com <shatyaki_dutt@hotmail.com>
2018-08-07 23:27:27 +0530

    After fixing dependencies

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
+const path = require('path');
 const app = express();
 // Middleware for body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,7 +43,15 @@ app.get("/", (req, res) => {
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
